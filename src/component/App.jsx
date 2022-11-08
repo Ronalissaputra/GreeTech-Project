5a2d5d0e8b1d43d7db0ac552f9da58ae7dc19b00
Macbook Air <ronalissaputra@gmail.com>
2022-11-08 16:39:38 +0800

    Update

---
+++ src/component/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./NavbarTech";
+import Footer from "./FooterTech";
+import HomeTech from "../pages/HomeTech";
+import ProductTech from "../pages/ProductTech";
+import ContactTech from "../pages/ContactTech";
+import AboutTech from "../pages/AboutTech";
+
+function App() {
+  return (
+    <div className="Note-app">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route index element={<HomeTech />} />
+          <Route path="/Product" element={<ProductTech />} />
+          <Route path="/Contact" element={<ContactTech />} />
+          <Route path="/About" element={<AboutTech />} />
+        </Routes>
+      </main>
+      <footer className="pt-5">
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
